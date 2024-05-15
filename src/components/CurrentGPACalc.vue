ef7c41922d9aa5924523ba85c667e61cf5610c19
Huajie Zhong <hz642@cornell.edu>
2024-05-15 04:01:39 -0400

    edit style

--- src/components/CurrentGPACalc.vue
+++ src/components/CurrentGPACalc.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
-    <h1>Current GPA Calculator</h1>
+    <h1 style="text-align: center">Current GPA Calculator</h1>
     <form>
       <div>
         <div v-for="(grade, index) in grades" :key="index" class="grade-row">
           <label v-if="index === 0" id="credit">Total Credits</label>
           <label v-if="index === 0" id="num-eq">Number Equivlent</label>
           <label v-if="index === 0" id="point-per-grade">Points per Grade</label>
-          <label :for="grade" class="grade-label">{{ grade }}:</label>
+          <label :for="grade" class="grade-label">{{ grade }}</label>
           <input
             :id="grade"
             v-model.number="gradeValues[grade]"
@@ -17,8 +17,8 @@
             required
             class="grade-input"
           />
-          <input class="num-eq" readonly />
-          <input class="point-per-grade" readonly />
+          <input class="num-eq-box" readonly />
+          <input class="point-per-grade-box" readonly />
         </div>
       </div>
     </form>
@@ -51,24 +51,63 @@ export default {
 <style scoped>
 .grade-row {
   display: grid;
-  grid-template-columns: 50px repeat(auto-fill, minmax(200px, 1fr));
-  grid-gap: 0.5rem;
+  grid-template-columns: 35px repeat(3, minmax(60px, 1fr));
+  grid-gap: 1rem;
 }
 .grapde-label {
   grid-column-start: 1;
   grid-column-end: 2;
   margin-bottom: 1rem;
 }
+
+.grade-input,
+.point-per-grade-box,
+.num-eq-box {
+  height: 2.5rem;
+  padding: 0 0.5rem;
+  font-size: 1rem;
+  border-radius: 15px;
+  margin-bottom: 5px;
+  background-color: var(--vt-c-black-soft);
+  border: none;
+  color: var(--vt-c-text-dark-2);
+  text-align: center;
+}
+
+.point-per-grade-box,
+.num-eq-box:focus {
+  outline: none;
+  pointer-events: none;
+}
+
+@keyframes border-pulse {
+  0% {
+    border-color: var(--vt-c-black-soft);
+  }
+  50% {
+    border-color: var(--vt-c-text-dark-2);
+  }
+  100% {
+    border-color: var(--vt-c-black-soft);
+  }
+}
+
+.grade-input:focus {
+  outline: none;
+  animation: border-pulse 2s infinite;
+  border-bottom: 2px solid;
+}
+
 .grade-input {
   grid-column-start: 2;
   grid-column-end: 3;
   align-items: center;
 }
-.point-per-grade {
+.point-per-grade-box {
   grid-column-start: 4;
 }
 
-.num-eq {
+.num-eq-box {
   grid-column-start: 3;
   height: 2.5rem;
 }
@@ -76,15 +115,26 @@ export default {
 #credit {
   grid-column-start: 2;
   margin-bottom: 1rem;
+  text-align: center;
 }
 
 #num-eq {
   grid-column-start: 3;
   margin-bottom: 1rem;
+  text-align: center;
 }
 
 #point-per-grade {
   grid-column-start: 4;
   margin-bottom: 1rem;
+  text-align: center;
+}
+
+input[type='number'].grade-input::-webkit-inner-spin-button,
+input[type='number'].grade-input::-webkit-outer-spin-button {
+  -webkit-appearance: none;
+  appearance: none;
+  background-color: transparent;
+  margin: 0;
 }
 </style>
