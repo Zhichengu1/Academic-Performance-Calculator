55382ed1407dfab2bb2c3e989caf8dee877d7dda
Huajie Zhong <hz642@cornell.edu>
2024-05-14 23:20:40 -0400

    add basic interface for future GPA calc

---
+++ src/components/FutureGPACalc.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+      <h1>Future GPA Calculator</h1>
+      <form @submit.prevent="calculateGPA">
+        <div>
+          <label for="gpa">Future GPA:</label>
+          <input id="gpa" v-model.number="gpa" type="number" step="0.01" min="0" max="4" required>
+        </div>
+        <div>
+          <label for="credits">Total Credits:</label>
+          <input id="credits" v-model.number="credits" type="number" min="0" required>
+        </div>
+        <button type="submit">Calculate</button>
+      </form>
+      <p v-if="result !== null">Your current GPA is: {{ result }}</p>
+    </div>
+  </template>
+
+<script>
+export default {
+  data() {
+    return {
+      gpa: null,
+      credits: null,
+      result: null
+    }
+  },
+  methods: {
+    calculateGPA() {
+      this.result = this.gpa;
+    }
+  }
+}
+</script>
