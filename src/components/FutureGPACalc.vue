e33af73988f826d3f59098ba2487f715d73e5e63
Zhiasw <yuzhicheng369@gmail.com>
2024-05-17 18:04:22 -0400

    the hover and spinner finish

--- src/components/FutureGPACalc.vue
+++ src/components/FutureGPACalc.vue
@@ -2,29 +2,32 @@
   <div>
     <h1 style = "text-align: center" >Future GPA Calculator</h1>
     <form @submit.prevent="calculateGPA">
-      <div class = "total_credit">
+
+      <div class = "table">
         <div v-for = "(label, index) in labels" :key="index">
           <label: for = "'input' + index"> {{label}} </label:>
           <input :id = "'input' + index" v-model.number = "inputs['input' + index]" type="number" min="0" required />
         </div>
       </div>
-
-      <div>
-
+      <div class = "calculateGPA">
+        <div v-for = "(label, index) in labels_gpa" :key="index">
+          <label: for = "'input' + index"> {{label}} </label:>
+          <input :id = "'input' + index" v-model.number = "inputs['input' + index]" type="number" min="0" required />
+        </div>
       </div>
-
       <button type="submit">Calculate</button>
-
-
     </form>
     <p v-if="result !== null">Your current GPA is: {{ result }}</p>
   </div>
 </template>
+
+
 <script>
 export default {
   data() {
     return {
-      labels: ['Total Credits:', 'Credits of F, I/F, and N/F Grade', 'Credits of P and S Grades', 'Total transfer and Test'],
+      labels: ['Total Credits:', 'Credits of F, I/F, and N/F Grade:', 'Credits of P and S:', 'Total transfer and Test:'],
+      labels_gpa: ['Current quality Points:', 'Anticipated credits:', 'Future quality points:'],
       inputs: {},
       gpa: null,
       earn_credits: null,
@@ -46,17 +49,35 @@ export default {
 </script>
 
 <style>
-
-.total_credits
+input
 {
-  text-align: left;
+  border-radius: 30px;
+  width: 80px;
+  height: 35px;
+  background-color: lightgray;
+  border-color: transparent;
 }
-
+input:hover
+{
+  background-color: lightslategray;
+}
 .calculateGPA
 {
   text-align: right;
 }
 
+.table
+{
+  display: grid;
+  grid-template-columns: auto auto auto auto; /* Creates four columns */
+  grid-template-rows: auto;
+}
+
+
+input[type="number"]::-webkit-inner-spin-button, 
+input[type="number"]::-webkit-outer-spin-button { 
+  -webkit-appearance: none; 
+}
 
 
 </style>
