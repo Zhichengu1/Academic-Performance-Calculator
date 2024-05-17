f3c25186d40733fe446947f0fae50ff61416130d
Zhiasw <yuzhicheng369@gmail.com>
2024-05-17 00:19:27 -0400

    total_creditinput complete

--- src/components/FutureGPACalc.vue
+++ src/components/FutureGPACalc.vue
@@ -2,40 +2,61 @@
   <div>
     <h1 style = "text-align: center" >Future GPA Calculator</h1>
     <form @submit.prevent="calculateGPA">
-      <div>
-        <label for="gpa">Future GPA:</label>
-        <input id="gpa" v-model.number="gpa" type="number" step="0.01" min="0" max="4" required />
+      <div class = "total_credit">
+        <div v-for = "(label, index) in labels" :key="index">
+          <label: for = "'input' + index"> {{label}} </label:>
+          <input :id = "'input' + index" v-model.number = "inputs['input' + index]" type="number" min="0" required />
+        </div>
       </div>
+
       <div>
-        <label for="credits">Credits of F, I/F, and N/F Grades</label>
+
       </div>
-      <div>
-        <label for="credits">Credits of P and S Grades</label>
-      </div>
-      <div>
-        <label for="credits">Total Credits:</label>
-        <input id="credits" v-model.number="credits" type="number" min="0" required />
-      </div>
+
       <button type="submit">Calculate</button>
+
+
     </form>
     <p v-if="result !== null">Your current GPA is: {{ result }}</p>
   </div>
 </template>
-
 <script>
 export default {
   data() {
     return {
+      labels: ['Total Credits:', 'Credits of F, I/F, and N/F Grade', 'Credits of P and S Grades', 'Total transfer and Test'],
+      inputs: {},
       gpa: null,
-      credits: null,
+      earn_credits: null,
+      total_credit: null,
       result: null
     }
   },
+
   methods: {
     calculateGPA() {
       this.result = this.gpa
+    },
+    calculateCredit()
+    {
+      this.total_credit = this.earn_credits
     }
   }
 }
-
 </script>
+
+<style>
+
+.total_credits
+{
+  text-align: left;
+}
+
+.calculateGPA
+{
+  text-align: right;
+}
+
+
+
+</style>
