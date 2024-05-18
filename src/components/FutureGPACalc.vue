b6a015dc0046c58b1584b681b1a69a9075915d95
Zhiasw <yuzhicheng369@gmail.com>
2024-05-18 14:52:14 -0400

    updated grid and table layout

--- src/components/FutureGPACalc.vue
+++ src/components/FutureGPACalc.vue
@@ -2,40 +2,45 @@
   <div>
     <h1 style = "text-align: center" >Future GPA Calculator</h1>
     <form @submit.prevent="calculateGPA">
+    <table>
+    <tr>
+      <td style = "color:black; background-color: lightgray;">I. Total credits used to calculate your cumulative GPA</td>
+    </tr>
+    <tr v-for="(label, index) in labels" :key="index">
+      <td><input :id="'input' + index" v-model.number="inputs['input' + index]" type="number" min="0" required /></td>
+      <td>{{ label }}</td>
+    </tr>
+    <tr>
+      <td>output</td>
+      <td>Total GPA credits*</td>
+    </tr>
+    </table>
+    <h2 style ="text-align: center">Calculate Your cumulative GPA</h2>
+    <div class ="grid-columns">
+    <div v-for = "(label, index) in labels_gpa" :key="index">
+      <label for = "'input' + index"> {{label}} </label>
+      <input :id = "'input' + index" v-model.number = "inputs['input' + index]" type="number" min="0" required />
+    </div>
+  </div>
 
-      <div class = "table">
-        <div v-for = "(label, index) in labels" :key="index">
-          <label: for = "'input' + index"> {{label}} </label:>
-          <input :id = "'input' + index" v-model.number = "inputs['input' + index]" type="number" min="0" required />
-        </div>
-      </div>
-      <div class = "calculateGPA">
-        <div v-for = "(label, index) in labels_gpa" :key="index">
-          <label: for = "'input' + index"> {{label}} </label:>
-          <input :id = "'input' + index" v-model.number = "inputs['input' + index]" type="number" min="0" required />
-        </div>
-      </div>
-      <button type="submit">Calculate</button>
     </form>
     <p v-if="result !== null">Your current GPA is: {{ result }}</p>
   </div>
 </template>
 
-
 <script>
 export default {
   data() {
     return {
-      labels: ['Total Credits:', 'Credits of F, I/F, and N/F Grade:', 'Credits of P and S:', 'Total transfer and Test:'],
-      labels_gpa: ['Current quality Points:', 'Anticipated credits:', 'Future quality points:'],
+      labels: [':Enter TOTAL earned credits:', ':Enter repeated credits NOT earned.', ':Enter TOTAL credits of F, I/F, and N/F grades', ':Enter TOTAL credits of P and S grades', ':Enter TOTAL transfer and test(AP, CLEP, CPE) creidts'],
       inputs: {},
+      labels_gpa: ['Current GPA credits: ', 'Current quality Points:', 'display', 'Anticipated credits:', 'Future quality points:', 'Display', 'Overall credits:', 'Total Quatlity points', 'Display'],
       gpa: null,
       earn_credits: null,
       total_credit: null,
       result: null
     }
   },
-
   methods: {
     calculateGPA() {
       this.result = this.gpa
@@ -49,35 +54,38 @@ export default {
 </script>
 
 <style>
-input
-{
-  border-radius: 30px;
-  width: 80px;
-  height: 35px;
-  background-color: lightgray;
-  border-color: transparent;
-}
+input,
 input:hover
 {
-  background-color: lightslategray;
+  width: auto;
+  height: 35px;
+  background-color: darkseagreen	;
+  border-color: transparent;
 }
-.calculateGPA
+.grid-columns {
+    display: grid; 
+    grid-template-columns: repeat(3, 1fr); /* Change this line */
+}
+table
 {
-  text-align: right;
+  border: 2px solid blueviolet;
+  border-width: auto;
 }
-
-.table
-{
-  display: grid;
-  grid-template-columns: auto auto auto auto; /* Creates four columns */
-  grid-template-rows: auto;
-}
-
 
 input[type="number"]::-webkit-inner-spin-button, 
 input[type="number"]::-webkit-outer-spin-button { 
   -webkit-appearance: none; 
 }
 
+/*  
+
+
+<div class = "calculateGPA">
+      <button type="submit">Calculate</button>
+        <div  v-for = "(label, index) in labels_gpa" :key="index">
+          <label: for = "'input' + index"> {{label}} </label:>
+          <input :id = "'input' + index" v-model.number = "inputs['input' + index]" type="number" min="0" required />
+        </div>
+      </div>*/
 
 </style>
