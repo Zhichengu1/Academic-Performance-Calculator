3eb6296af164443732a76d02e8e9b9261461f114
Huajie Zhong <hz642@cornell.edu>
2024-05-20 00:56:23 -0400

    adjust button animation style
    -add ease in and out for box glow

--- src/App.vue
+++ src/App.vue
@@ -62,29 +62,6 @@ main {
     flex-wrap: wrap;
   }
 }
-@keyframes shake
-{
-  10%,
-  90% {
-    transform: translate3d(-2px, 0, 0);
-  }
-
-  40%,
-  80% {
-    transform: translate3d(1px, 0, 0);
-  }
-
-  30%,
-  50%,
-  70% {
-    transform: translate3d(-2px, 0, 0);
-  }
-
-  40%,
-  60% {
-    transform: translate3d(2px, 0, 0);
-  }
-}
 
 #container {
   width: 50rem;
@@ -129,9 +106,10 @@ main {
   }
   &:hover {
     background: var(--primary-color);
-    transition: background 0.8s ease;
+    transition:
+      background 0.8s ease,
+      box-shadow 0.6s ease;
     box-shadow: 0 0 20px 10px hsla(204, 70%, 53%, 0.5);
-    animation: shake 0.82s both;
     &::before {
       transform: translateX(100%);
     }
